d35838770a116f323530579a12276654489b997e
DuckVN <149903337+quocvietpham185@users.noreply.github.com>
2025-03-08 17:29:17 +0700

    Update index.ts with PORT= 3000

--- src/index.ts
+++ src/index.ts
@@ -234,7 +234,7 @@ app.delete('/users/:id', async (req, res) => {
   }
 });
 
-const PORT = 3002;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   console.log(`Swagger documentation available at http://localhost:${PORT}/api-docs`);
